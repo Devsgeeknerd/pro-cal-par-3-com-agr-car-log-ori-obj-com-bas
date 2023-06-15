179a04aa6afd5c41dcc7fda1001fd5173fe4b0ac
Paulo Vanderley <vanderley.1109@gmail.com>
2023-06-15 14:41:59 -0300

    adicionando conteudo

--- Multiplicacao.java
+++ Multiplicacao.java
@@ -1,3 +1,12 @@
-public class Multiplicacao {
+package br.com.devsgeeknerd.calculadora.classes;
 
+public class Multiplicacao extends CalculoMatematico {
+    public Multiplicacao(int numero1, int numero2) {
+        super(numero1, numero2);
+    }
+
+    @Override
+    protected int doCalcular() {
+        return numero1 * numero2;
+    }
 }
